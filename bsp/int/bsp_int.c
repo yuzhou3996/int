843c98d4f9396a95d5326373e131bea2e7318c57
windows.yuzhou <null>
2020-02-12 21:18:34 +0800

    提交修改

--- bsp/int/bsp_int.c
+++ bsp/int/bsp_int.c
@@ -1,5 +1,26 @@
 #include "bsp_int.h"
 
+/* 中断处理函数表 */
+static sys_irq_handle_t irqTable[NUMBER_OF_INT_VECTORS];
+
+/* 初始化中断处理函数表 */
+void system_irqtable_init()
+{
+    unsigned int i = 0;
+    for (i = 0; i < NUMBER_OF_INT_VECTORS; i++)
+    {
+        irqTable[i].irqHandler = default_irqhandler;
+        irqTable[i].userParam = NULL;
+    }
+}
+
+/* 注册中断处理函数 */
+void system_register_irqhandler(IRQn_Type irq, system_irq_handler_t handler, void *userParam)
+{
+    irqTable[irq].irqHandler = handler;
+    irqTable[irq].userParam = userParam;
+}
+
 /* 中断初始化函数 */
 void int_init()
 {
@@ -7,5 +28,20 @@ void int_init()
 
     /* 中断向量偏移设置 */
     __set_VBAR(0x87800000);
+}
+
+/* 具体的中断处理函数 IRQ_Handler会调用此函数 */
+void system_irqhandler(unsigned int gicciar)
+{
     
+}
+
+
+
+/* 默认中断处理函数 */
+void default_irqhandler(unsigned int gicciar, void *userParam)
+{
+    while (1)
+    {
+    }
 }
--- bsp/int/bsp_int.h
+++ bsp/int/bsp_int.h
@@ -2,7 +2,20 @@
 #define _BSP_INT_H
 #include "imx6ull.h"
 
+/* 函数声明 */
 void int_init();
+void default_irqhandler(unsigned int gicciar, void *userParam);
+void system_irqtable_init();
+void system_register_irqhandler(IRQn_Type irq, system_irq_handler_t handler, void *userParam);
 
 
-#endif 
+typedef void (*system_irq_handler_t)(unsigned int gicciar, void *param);
+
+/* 中断处理函数结构体 */
+typedef struct _sys_irq_handle
+{
+    system_irq_handler_t irqHandler; /* 中断处理函数 */
+    void *userParam;                 /* 中断处理函数的参数 */
+} sys_irq_handle_t;
+
+#endif
